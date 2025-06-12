c39edd325cad320ace33dcc4e26da74a823345db
Roman Grigoriev <roman@curlybr.com>
2025-06-12 23:33:33 +0700

    init

---
+++ src/main/java/ru/planetmc/quoctor/QuoctorApplication.java
@@ -0,0 +1,13 @@
+package ru.planetmc.quoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoctorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuoctorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/planetmc/quoctor/QuoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.planetmc.quoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoctorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
